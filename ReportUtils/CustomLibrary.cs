5e30f38991a79dee68e772954b6bfd9174e62bf3
Sivarama Garlapati <Sivarama.Garlapati@mass.gov>
2020-06-04 15:35:04 -0400

    Add project files.

---
+++ ReportUtils/CustomLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security;
+using System.Runtime;
+
+
+[assembly: AllowPartiallyTrustedCallers]
+
+
+    public sealed class ReportUtils
+    {
+        public static string ReportFooter()
+        {
+            return "Printed " +
+                DateTime.Now.ToString("dd-MMM-yyyy HH:mm") +
+                " by " + Environment.UserName;
+        }
+    }
